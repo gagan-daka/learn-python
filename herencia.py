6703e5902a0bde2cddda62feeab9a72be7137ff7
gagan-daka <gagandeps0@gmail.com>
2021-08-30 15:57:44 +0200

    herencia

--- herencia.py
+++ herencia.py
@@ -10,10 +10,34 @@ gagan = Persona("Gagan", 21, "12345678X")
 gagan.presentarse()
 print(gagan.DNI)
 
-#HERENCIA
-class Trabajador(Persona):
-    pass
+#HERENCIA -> class 'nombreClasse'(clase a heredar):
+class Trabajador(Persona):    
+    def __init__(self, nombre, edad, DNI, sueldo, cargo, empresa):
+        super().__init__(nombre, edad, DNI) #esto hace referencia al metodo init de la classe Persona
+        self.sueldo = sueldo
+        self.cargo = cargo
+        self.empresa = empresa
 
-trabajador = Trabajador("Juan", 33, "14785236Z")
+    def calcularSueldoAnual(self):
+        self.pagaExtra = 2000
+        return 12*self.sueldo + self.pagaExtra
+
+class Estudiante(Persona):
+    def __init__(self, nombre, edad, DNI, universidad, curso, assignaturas):
+        super().__init__(nombre, edad, DNI)
+        self.universidad = universidad
+        self.curso = curso
+        self.assignaturas = assignaturas
+
+    def describirse(self):
+        print(f"""¡Hola soy {self.nombre}!. Tengo {self.edad} años y estudio en la universidad {self.universidad}.
+        Estoy en el curso {self.curso}""")
+
+trabajador = Trabajador("Juan", 33, "14785236Z", 1500, "machaca", "HeyHay")
 trabajador.presentarse()
 print(trabajador.DNI)
+print(trabajador.calcularSueldoAnual())
+
+
+estudiante = Estudiante("Ana", 21, "00000000Z", "UAB", 2, ["Cálculo", "Finanzas", "Álgebra"])
+estudiante.describirse()
