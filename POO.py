7ed6a515ea4a7a7626793bf243f84e4acab452aa
gagan-daka <gagandeps0@gmail.com>
2021-08-30 14:16:52 +0200

    POO

---
+++ POO.py
@@ -0,0 +1,50 @@
+'''
+PILARES DE LA PROGRAMACION ORIENTADA A OBTEJOS (P00)
+- HERENCIA
+- POLIMORFISMO
+- ENCAPSULACIÓN
+- ABSTRACCIÓN
+
+AL CREAR METODOS(funciones) DENTRO DE LA CLASSE
+SIEMPRE HAY QUE PASARLE EL 'SELF' POR ()
+'''
+
+
+class Camiseta:
+    
+    #CONSTRUCTOR
+    def __init__(self, marca, precio, talla, color): 
+        self.marca = marca
+        self.precio =precio
+        self.talla = talla
+        self.color = color
+        self.rebaja = False
+
+
+    def aplicarDescuento(self, porcentaje):
+        self.precio = self.precio - self.precio*porcentaje/100
+        if porcentaje < 100:
+            self.rebaja = True    
+
+    def infoProducto(self):
+        info = f"Descripción de la camiseta:\nMarca: {self.marca}\nPrecio: {self.precio:.2F}€\nTalla: {self.talla}\nColor: {self.color}"
+        if self.rebaja:
+            info += "\nESTE PRODUCTO ESTÁ REBAJADO"
+        return info
+
+#INSTANCIAMOS LA CLASSE
+camiseta = Camiseta("Nike", 19.99, "S", "Lila")
+camisetaAdidas = Camiseta("Adidas", 30, "M", "Negra")
+
+print(camisetaAdidas.precio)
+camisetaAdidas.aplicarDescuento(50)
+print(camisetaAdidas.precio)
+
+camiseta.aplicarDescuento(20)
+print(camiseta.precio)
+
+print(camiseta.infoProducto())
+print("####################")
+print(camisetaAdidas.infoProducto())
+
+https://www.youtube.com/watch?v=uMfxoErGUS8&list=PLVs9JdE6ZZh112EBnYa_wCxYpbVXEXrLF&index=14
